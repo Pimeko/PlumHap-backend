59d6d56dc0583f90fd32010f8a857aa53ba61bf4
Pimeko <benjamin.widaw@gmail.com>
2017-11-26 17:34:26 +0100

    add cors headers

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ var auth_middleware = require('./middleware/AuthMiddleware');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.route('/')
   .get(test_middleware.default, function(req, res) {
