b9c1a03b52e12105a9c84c8d7527dbfba8ba09ec
Thibault Virsolvy <thibault.virsolvy@epita.fr>
2017-11-26 19:55:53 +0100

    added the tolower parce qu'il le faut bien

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var auth_middleware = require('./middleware/AuthMiddleware');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'.toLowerCase());
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
@@ -32,36 +32,36 @@ app.use(function (req, res, next) {
 app.use('/api', router);
 
 router.route('/')
-  .get(test_middleware.default, function(req, res) {
-    res.send("Everything is fine !")
-  });
+    .get(test_middleware.default, function(req, res) {
+        res.send("Everything is fine !")
+    });
 
 router.route('/statements')
-  .get(auth_middleware.default, statement_controller.all)
-  .post(auth_middleware.default, statement_controller.post);
+    .get(auth_middleware.default, statement_controller.all)
+    .post(auth_middleware.default, statement_controller.post);
 
 router.route('/statements/:id')
-  .get(auth_middleware.default, statement_controller.get)
-  .put(auth_middleware.default, statement_controller.edit)
-  .delete(auth_middleware.default, statement_controller.delete);
+    .get(auth_middleware.default, statement_controller.get)
+    .put(auth_middleware.default, statement_controller.edit)
+    .delete(auth_middleware.default, statement_controller.delete);
 
 router.route('/activities')
-  .get(auth_middleware.default, activity_controller.all)
-  .post(auth_middleware.default, activity_controller.post);
+    .get(auth_middleware.default, activity_controller.all)
+    .post(auth_middleware.default, activity_controller.post);
 
 router.route('/activities/:id')
-  .get(auth_middleware.default, activity_controller.get)
-  .put(auth_middleware.default, activity_controller.edit)
-  .delete(auth_middleware.default, activity_controller.delete);
+    .get(auth_middleware.default, activity_controller.get)
+    .put(auth_middleware.default, activity_controller.edit)
+    .delete(auth_middleware.default, activity_controller.delete);
 
 router.route('/login')
-  .post(user_controller.login);
+    .post(user_controller.login);
 
 router.route('/users/:id')
-  .put(auth_middleware.correctUser, user_controller.update);
+    .put(auth_middleware.correctUser, user_controller.update);
 
 console.log("\u001b[2J\u001b[0;0H");
 console.log('Running Server on port 8000 ! '.yellow);
 app.listen(8000, function() {
-  db.sequelize.sync();
+    db.sequelize.sync();
 });
